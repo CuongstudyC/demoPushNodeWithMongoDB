a363a7e8ff6dd08d5539a469e47c4e20c81eea2c
CuongstudyC <113528964+CuongstudyC@users.noreply.github.com>
2024-03-13 18:05:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://cuong979899:cuocsongma123@validate.bbwogpc.mongodb.net/validate?retryWrites=true&w=majority&appName=validate');
+mongoose.connect('mongodb+srv://cuong979899:cuocsongma123@validate.bbwogpc.mongodb.net/validate?retryWrites=true&w=majority&appName=validate',{useNewUrlParser: true});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
